1909cc13f18c38dc5b98f4d1426a5b35454f99df
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2025-04-03 18:44:13 +0330

    new commit!

--- Chaining_operators.py
+++
@@ -1,4 +0,0 @@
-a = int(input("Num: "))
-b = int(input("Num: "))
-c = int(input("Num: "))
-print (a < b <= c)
--- Fruit_vocabulary_practice.py
+++
@@ -1,7 +0,0 @@
-fruit_prices = {"Apple": 1500,
-                "Banana": 1000,
-                "Orange": 1200}
-
-fruit_prices["Banana"] = 1100
-fruit_prices.pop("Apple")
-print(fruit_prices)
