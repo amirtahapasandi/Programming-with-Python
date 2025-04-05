765d4114029c942344d0a1ab017cf6e22887e392
AmirTaha <amirtaha.1387.pasandi@gmail.com>
2025-04-05 23:44:55 +0330

    new commit.

--- Conditional_tore.py
+++ Conditional_tore.py
@@ -1,10 +1,10 @@
 purchase_amount = int(input("How mush is your purchase? "))
 if purchase_amount > 50000:
-    discount_amount = (purchase_amount / 5)
+    discount_amount = (purchase_amount // 5)
     new_amount = purchase_amount - discount_amount
     print(new_amount)
 elif 2000 < purchase_amount < 50000:
-    discount_amount = (purchase_amount / 10)
+    discount_amount = (purchase_amount // 10)
     new_amount = purchase_amount - discount_amount
     print(new_amount)
 else:
